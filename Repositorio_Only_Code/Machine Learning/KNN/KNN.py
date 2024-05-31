9b79daaf75c14183d5b542fc43f8136b28b71954
José Luis López Carmona <joselu4000@gmail.com>
2024-05-31 13:18:26 +0200

    Update KNN.py
    Update comments

--- Repositorio_Only_Code/Machine Learning/KNN/KNN.py
+++ Repositorio_Only_Code/Machine Learning/KNN/KNN.py
@@ -1,3 +1,8 @@
+# Python 3.11.5
+# FILE: KNN.py
+# AUTHOR: José Luis López Carmona
+# CREATE DATE: 21/05/2024
+
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
@@ -6,13 +11,14 @@ from sklearn.metrics import classification_report
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.utils import to_categorical
 from imblearn.over_sampling import SMOTE
+
 #############################################################
 import time
-
-# Guarda el tiempo de inicio
+# Saving initial time
 start_time = time.time()
 #############################################################
 
+# Load data
 def load_data(input_file):
     data = pd.read_csv(input_file)
     labels = data['seq_id'].str.split('_').str[1]
@@ -21,9 +27,11 @@ def load_data(input_file):
     data = data.apply(pd.to_numeric, errors='coerce')
     return labels, data
 
+# Standard data
 def normalize(input_data):
     return input_data / np.max(input_data)
 
+# Encoder data
 def prepare_data(clase, taxonomy_file, labels, data):
     taxonomy = pd.read_csv(taxonomy_file)
     Taxon_L = taxonomy.shape[1]
@@ -38,17 +46,24 @@ def prepare_data(clase, taxonomy_file, labels, data):
     data_n = normalize(data)
     return data_n, y_encoded
 
+# My own paths
 input_file = r'C:\Users\JoseLuisLopezCarmona\Documents\MCD\TFM\Codigo\datos\SGk4.csv'
 input_micro = r'C:\Users\JoseLuisLopezCarmona\Documents\MCD\TFM\Codigo\datos\taxonomy.csv'
+
+# Prepare and encoding data
 labels, data = load_data(input_file)
 data_n, y_encoded = prepare_data('Class', input_micro, labels, data)
+
+# Division data
 X_train, X_val, y_train, y_val = train_test_split(data_n, y_encoded, test_size=0.1, 
                                                   stratify=np.argmax(y_encoded, axis=1), 
                                                   random_state=42)
 
+# Using SMOTE for a balanced division
 smote = SMOTE(random_state=42)
 X_train, y_train = smote.fit_resample(X_train, y_train)
 
+# Grid to search best params within KNN
 param_grid = {
     'n_neighbors': [3, 5, 7],
     'weights': ['uniform', 'distance'],
@@ -56,6 +71,7 @@ param_grid = {
     'p': [1, 2]
 }
 
+# Model, train and metrics
 knn = KNeighborsClassifier()
 grid_search = GridSearchCV(estimator=knn, param_grid=param_grid, 
                            cv=2, verbose=1, n_jobs=1) 
