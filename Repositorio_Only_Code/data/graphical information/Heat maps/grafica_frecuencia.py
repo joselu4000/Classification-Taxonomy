1f63481f3a4b82f90589083e893915be2bea1576
José Luis López Carmona <joselu4000@gmail.com>
2024-05-31 13:33:31 +0200

    Update grafica_frecuencia.py
    Update comments

--- Repositorio_Only_Code/data/graphical information/Heat maps/grafica_frecuencia.py
+++ Repositorio_Only_Code/data/graphical information/Heat maps/grafica_frecuencia.py
@@ -1,35 +1,43 @@
+# Python 3.11.5
+# FILE: Heat_maps.py
+# AUTHOR: José Luis López Carmona
+# CREATE DATE: 29/05/2024
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 def load_data(input_file):
-    # Leer el archivo CSV, usando la primera columna (seq_id) como índice
+    # Load CSV
     data = pd.read_csv(input_file)
     
-    # Extraer y limpiar los identificadores de la columna 'seq_id'
+    # Clean
     labels = data['seq_id'].str.split('_').str[1]
     
-    # Establecer los identificadores limpios como índice del DataFrame
+    # Reindex
     data.set_index(labels, inplace=True)
-    
-    # Eliminar la columna original 'seq_id' ya que ahora es el índice
     data.drop('seq_id', axis=1, inplace=True)
     
-    # Convertir todas las columnas restantes a numérico, gestionando errores por si acaso
+    # Convert rest colmns to numericall
     data = data.apply(pd.to_numeric, errors='coerce')
 
     return labels, data
 
 def normalize (input_data):
+    # Standard data
     maximum = np.max(input_data)
     data_normalize = input_data/maximum
     return(data_normalize)
 
+# My own path
 input_file = r'C:\Users\JoseLuisLopezCarmona\Documents\MCD\TFM\Codigo\datos\AMPk3.csv'
+
+# Labels and data
 labels, data = load_data(input_file)
 data = normalize(data)
 
+# Heat-map
 plt.figure(figsize=(18, 12))
 sns.heatmap(data.iloc[:, 1:], cmap='viridis', yticklabels=False)
 plt.title('Mapa Calor k-mers AMP, k=3')
