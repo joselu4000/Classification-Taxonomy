0af8feb0038dddce7c5503d868f7b18c0382a959
José Luis López Carmona <joselu4000@gmail.com>
2024-05-31 13:28:47 +0200

    Update texto.py
    Update comments

--- Repositorio_Only_Code/data/preprocessing/texto.py
+++ Repositorio_Only_Code/data/preprocessing/texto.py
@@ -1,5 +1,5 @@
 # Python 3.11.5.
-# FILE: CNN_propia.py
+# FILE: texto.py
 # AUTHOR: José Luis López Carmona
 # CREATE DATE: 28/01/2024
 from Bio import SeqIO
@@ -7,18 +7,15 @@ import sys
 
 def extract_full_taxonomy_info(fasta_file_path, output_file_path):
     with open(output_file_path, 'w') as output_file:
-        # Escribir cabecera del archivo de salida para columnas
         output_file.write("ID, Phylum, Order, Family, Genus\n")
         
         for record in SeqIO.parse(fasta_file_path, "fasta"):
-            header_parts = record.description.split()  # Dividir la descripción en partes
-            # Asegurarse de que todos los componentes esperados están presentes
-            if len(header_parts) >= 5:
+            header_parts = record.description.split()  
+            if len(header_parts) >= 5:    # Example:
                 phylum = header_parts[1]  # Alphaproteobacteria
                 order = header_parts[2]   # Rhodobacterales
                 family = header_parts[3]  # Rhodobacteraceae
                 genus = header_parts[4]   # Rhodobaca
-                # Escribir al archivo de salida
                 output_file.write(f"{record.id}, {phylum}, {order}, {family}, {genus}\n")
             else:
                 print(f"Warning: Missing data for record {record.id}")
@@ -32,4 +29,5 @@ if __name__ == "__main__":
     output_text_file = sys.argv[2]
     extract_full_taxonomy_info(input_fasta_file, output_text_file)
 
+# Use this code into bash:    
 # python texto.py data\Milseq.fasta micro.txt
